7bfb9ec375c67ffe71b91db6b3c37abcfb596f84
omarsalim <83906211+Everezze@users.noreply.github.com>
2024-04-17 02:01:26 +0200

    added bash lsp config, remove html snippets for vanilla js

--- lua/my_lsp_config.lua
+++ lua/my_lsp_config.lua
@@ -27,7 +27,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'html', 'cssls', 'tsserver', 'emmet_ls', 'vimls', 'pyright', 'intelephense','clangd','lua_ls'}
+local servers = { 'html', 'cssls', 'tsserver', 'emmet_ls', 'vimls', 'pyright', 'intelephense','clangd','lua_ls','bashls'}
 
 
 
@@ -43,7 +43,8 @@ for _, lsp in pairs(servers) do
 end
 
 require('lspconfig')['emmet_ls'].setup{
-    filetypes = {'javascript','html','javascriptreact','css','typescript'}
+    filetypes = {'html','javascriptreact','css','typescript'}
+    --filetypes = {'javascript','html','javascriptreact','css','typescript'}
 }
 
 require('lspconfig')['tsserver'].setup{
